cd2e577aa22825e2e0d6178f419a7abfa478b2a6
German <jgerman_ob@hotmail.com>
2017-09-29 14:22:27 -0500

    Guarda los contactos seleccionados por el usuario

--- app/src/main/java/com/germanco/contactos/MainActivity.java
+++ app/src/main/java/com/germanco/contactos/MainActivity.java
@@ -6,6 +6,8 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,19 +19,28 @@ public class MainActivity extends AppCompatActivity {
     public ListView listaContactos;
     Cursor contactoCursor;
     AdaptadorContacto adaptadorContacto;
+    Button botonGuardar;
+    public List<Contacto> contactosGuardados= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listaContactos= (ListView)findViewById(R.id.contactsList);
+        botonGuardar=(Button)findViewById(R.id.botonGuardar);
         adaptadorContacto= new AdaptadorContacto(this,contactoList);
         listaContactos.setAdapter(adaptadorContacto);
         obtenerContactos();
+        botonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Estado","Guardar contactos");
+                guardarContactos();
+            }
+        });
     }
 
     public void obtenerContactos(){
-        Uri contactsUri=ContactsContract.Data.CONTENT_URI;
         String[] projection= new String[]{ContactsContract.Data._ID,ContactsContract.Data.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.TYPE};
         String where= ContactsContract.Data.MIMETYPE+"='"+ ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE+"' AND "+ ContactsContract.CommonDataKinds.Phone.NUMBER+ " IS NOT NULL";
         String orden= ContactsContract.Data.DISPLAY_NAME+" ASC";
@@ -40,10 +51,20 @@ public class MainActivity extends AppCompatActivity {
                 contacto= new Contacto();
                 contacto.setNombre(contactoCursor.getString(1));
                 contacto.setTelefono(contactoCursor.getString(2));
+                contacto.setSelected(false);
                 contactoList.add(contacto);
                 adaptadorContacto.notifyDataSetChanged();
             }catch (NullPointerException ex){
                 Log.d("Estado",contactoCursor.getString(0));
+            }
+        }
+    }
+
+    public void guardarContactos(){
+        for(int i=0; i<AdaptadorContacto.contactoList.size();i++){
+            if(AdaptadorContacto.contactoList.get(i).getSelected()){
+                contactosGuardados.add(contactoList.get(i));
+                Log.d("Contacto guardado",contactoList.get(i).getNombre());
             }
         }
     }
