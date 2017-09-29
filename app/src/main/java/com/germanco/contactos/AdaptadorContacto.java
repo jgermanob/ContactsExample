f6c8b3d1de7c654e9fc0ac823a43effa1d34702c
German <jgerman_ob@hotmail.com>
2017-09-29 14:31:36 -0500

    Guarda los contactos seleccionados por el usuario

--- app/src/main/java/com/germanco/contactos/AdaptadorContacto.java
+++ app/src/main/java/com/germanco/contactos/AdaptadorContacto.java
@@ -2,10 +2,12 @@ package com.germanco.contactos;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,7 +15,7 @@ import java.util.List;
 
 public class AdaptadorContacto extends BaseAdapter {
     Activity activity;
-    List<Contacto> contactoList;
+   public static List<Contacto> contactoList;
     LayoutInflater inflater;
     public AdaptadorContacto(Activity activity, List<Contacto> contactoList){
         this.activity=activity;
@@ -35,7 +37,8 @@ public class AdaptadorContacto extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
         if(inflater==null){
             inflater=(LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
@@ -45,10 +48,27 @@ public class AdaptadorContacto extends BaseAdapter {
 
         TextView nombreContacto=(TextView)convertView.findViewById(R.id.nombreContacto);
         TextView telefonoContacto=(TextView)convertView.findViewById(R.id.telefonoContacto);
-
+        CheckBox seleccionContacto=(CheckBox)convertView.findViewById(R.id.checkBoxContacto);
         Contacto contacto= contactoList.get(position);
         nombreContacto.setText(contacto.getNombre());
         telefonoContacto.setText(contacto.getTelefono());
+        seleccionContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    if(contactoList.get(position).getSelected()){
+                        contactoList.get(position).setSelected(false);
+                        Log.d("Estado","Quitaste seleción de "+contactoList.get(position).getNombre());
+                    }else{
+                        contactoList.get(position).setSelected(true);
+                        Log.d("Estado","Selecionaste a "+contactoList.get(position).getNombre());
+                    }
+                }catch (NullPointerException ex){
+                    Log.d("Estado Exception",contactoList.get(position).getNombre());
+                }
+
+            }
+        });
         return convertView;
     }
 }
--- app/src/main/java/com/germanco/contactos/Contacto.java
+++ app/src/main/java/com/germanco/contactos/Contacto.java
@@ -3,10 +3,9 @@ package com.germanco.contactos;
 public class Contacto {
     String nombre;
     String telefono;
+    Boolean isSelected;
 
-    public String getNombre() {
-        return nombre;
-    }
+    public String getNombre() {return nombre;}
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -19,4 +18,8 @@ public class Contacto {
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+
+    public Boolean getSelected() {return isSelected;}
+
+    public void setSelected(Boolean selected) {isSelected = selected;}
 }
